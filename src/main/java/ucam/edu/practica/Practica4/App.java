8509a72eba71ac3d3ce9c64f0d9d9c424c20b603
jesusASIR <jesus.clase.g.p@gmail.com>
2019-12-06 22:34:38 +0100

    Add files via upload

---
+++ src/main/java/ucam/edu/practica/Practica4/App.java
@@ -0,0 +1,13 @@
+package ucam.edu.practica.Practica4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
